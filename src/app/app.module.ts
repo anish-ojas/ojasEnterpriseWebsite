1ba9bee99a4625e31621d8553bc7c5402f8ec8c8
Anish <81043438+anish-ojas@users.noreply.github.com>
2021-09-15 14:37:32 +0530

    Smooth Scroll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomepageComponent } from './homepage/homepage.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomepageComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RippleModule
+    RippleModule,
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
